9d7e1cc8d6d45b3eae53b1cbd93948024da4ce99
Aayanjit <aayanjit@ekoinfomatics.com>
2023-02-21 22:59:48 +0530

    order edit page home commit

--- src/pages/OrdersEditPage.jsx
+++ src/pages/OrdersEditPage.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 import invoice_icon_static from "../assets/img/sidebar/invoice_icon_static.svg";
 import delete_icon from "../assets/icons/delete_icon.svg";
 import item from '../assets/img/mock-images/about-us.png'
+import arrow_down from '../assets/icons/arrow-small-down-white.svg'
+import arrow_up from '../assets/icons/arrow-up-white.svg'
 
 const OrdersEditPage = () => {
 
   const [mainDropDown, setMainDropdown] = useState(false);
 
   const [orderData, setOrderData] = useState({});
+
+  const [statusDropdownToggle, setStatusDropdownToggle] = useState(false);
 
   const submitPageData = () => {
     console.log('')
@@ -53,10 +57,10 @@ const OrdersEditPage = () => {
         phone_number: '7789445698',
       },
       status_list: [
-        {status_name: 'Delivered', status_color: '#00ac69'},
-        {status_name: 'Dispatched', status_color: '#303030'},
-        {status_name: 'Canceled', status_color: '#FF0000'},
-        {status_name: 'Returned', status_color: '#e99f15'},
+        { status_name: 'Delivered', status_color: '#00ac69' },
+        { status_name: 'Dispatched', status_color: '#303030' },
+        { status_name: 'Canceled', status_color: '#FF0000' },
+        { status_name: 'Returned', status_color: '#e99f15' },
       ],
     })
   }, [])
@@ -64,10 +68,12 @@ const OrdersEditPage = () => {
 
   return (
     <div className='w-full p-5 pt-0'>
-      <div className='w-[90%] mx-auto pt-5'>
+      <div className='w-full'>
 
         {/* header */}
         <div className="flex justify-between items-start md:items-center sticky top-0 py-5">
+
+
           <div className='flex flex-col md:flex-row justify-start items-start md:items-center gap-5'>
             <h1 className="text-xl ">
               Order{" "}
@@ -77,16 +83,17 @@ const OrdersEditPage = () => {
             </h1>
             <div className='flex flex-col md:flex-row justify-center items-start md:items-center gap-5'>
               <div>
-                <h1 className='text-[14px] bg-opacity-5 py-[5px] px-2 w-full text-center bg-[white] text-[#00ac69] rounded-lg border '>Delivered</h1>
+                <h1 className='text-[13px] bg-opacity-5 py-[5px] px-2 w-full text-center bg-[white] text-[#00ac69] rounded-lg border '>Delivered</h1>
               </div>
               <div className='w-[1px] h-[15px] bg-gray-400 hidden md:block'></div>
-              <div className='flex items-end gap-2'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+              <div className='flex items-start gap-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                 </svg>
-                <h1 className='text-[13px]'>06.22.2019 at 01:36pm</h1></div>
+                <h1 className='text-[12px] text-gray-600'><span className='pr-2'>06.22.2019</span><span className='pr-2'>at</span><span className=''>01:36pm</span></h1></div>
             </div>
           </div>
+
 
           <div className='flex justify-center items-center gap-3 relative'>
             <button className="px-4 text-[19px] py-[5px] rounded-xl bg-opacity-90 hover:bg-opacity-100 transition-all active:scale-95 bg-[#208a48] text-white" onClick={() => setMainDropdown(!mainDropDown)}>
@@ -94,7 +101,7 @@ const OrdersEditPage = () => {
             </button>
 
             {/* delete popup */}
-            <div className={`w-full z-[100] absolute top-[100%] right-[82%] overflow-hidden p-0 transition-all duration-200 rounded-xl outline-none bg-white ${mainDropDown ? 'max-h-[400px] ease-in shadow-xl' : 'max-h-0 ease-out'}`}>
+            <div className={`w-full z-[550] absolute top-[100%] right-[82%] overflow-hidden p-0 transition-all duration-200 rounded-xl outline-none bg-white ${mainDropDown ? 'max-h-[400px] ease-in shadow-xl' : 'max-h-0 ease-out'}`}>
               <div className='text-[14px] border-b p-3 text-gray-400 hover:text-gray-600 hover:bg-gray-50 transition-all flex justify-start items-center gap-5 cursor-pointer' onClick={() => setMainDropdown(false)}>
                 <img src={invoice_icon_static} className=' w-[16px]' alt="" />
                 <h1 className=' leading-none'>View invoice</h1>
@@ -104,7 +111,7 @@ const OrdersEditPage = () => {
                 <h1 className=' leading-none'>Delete</h1>
               </div>
             </div>
-            <div className={`fixed bg-black opacity-30 inset-0 z-[80] transition-all duration-200 ${mainDropDown ? 'block' : 'hidden'}`} onClick={() => setMainDropdown(false)}>
+            <div className={`fixed bg-black opacity-30 inset-0 z-[540] transition-all duration-200 ${mainDropDown ? 'block' : 'hidden'}`} onClick={() => setMainDropdown(false)}>
 
             </div>
 
@@ -112,6 +119,9 @@ const OrdersEditPage = () => {
               SAVE CHANGES
             </button>
           </div>
+
+
+
         </div>
 
         {/* main flex */}
@@ -122,11 +132,16 @@ const OrdersEditPage = () => {
           <div className='w-full md:w-[65%] flex flex-col gap-5 md:gap-8'>
 
             {/* items */}
-            <div className='w-full bg-white shadow-md border rounded-[10px] p-4'>
+            <div className='w-full bg-white shadow-md border rounded-[10px] pb-1'>
+
+              {/* table */}
               <div className='w-full pt-5'>
-                <div className=' grid grid-cols-[45%_1fr_1fr_1fr] border-b pb-2 justify-center items-center'>
+
+
+                {/* table heading */}
+                <div className=' grid grid-cols-[45%_1fr_1fr_1fr] border-b pb-2 justify-center items-center px-4'>
                   <div className='flex justify-start items-center'>
-                    <h1 className='text-[12px] pl-5'>Products</h1>
+                    <h1 className='text-[16px] font-[500]'>Products</h1>
                   </div>
                   <div className='flex justify-end items-center'>
                     <h1 className='text-[12px]'>Unit Price</h1>
@@ -138,58 +153,62 @@ const OrdersEditPage = () => {
                     <h1 className='text-[12px]'>Item total</h1>
                   </div>
                 </div>
-                <div className='w-full max-h-[400px] overflow-y-scroll'>
+
+                {/* table data */}
+                <div className='w-full min-h-[255px] pt-2 '>
                   {
                     orderData?.items?.map((data, i) => {
                       return (
-                        <div className='w-full bg-white grid grid-cols-[45%_1fr_1fr_1fr] justify-center items-center py-2' key={i}>
+                        <div className='w-full bg-white grid grid-cols-[45%_1fr_1fr_1fr] justify-center items-start py-2 px-4' key={i}>
                           <div className='flex justify-start items-center gap-3'>
                             <div className='w-fit'>
                               <img src={data?.image} className='w-[65px]' alt="" />
                             </div>
                             <div className='flex flex-col justify-center items-start gap'>
-                              <h1 className='text-[14px] font-[500]'>{data?.title}</h1>
-                              <h1 className='text-[12px]'>{data?.category}</h1>
-                              <h1 className='text-[12px]'>{data?.size}</h1>
+                              <h1 className='text-[14px] font-[500] text-gray-700'>{data?.title}</h1>
+                              <h1 className='text-[12px] text-gray-400'>{data?.category}</h1>
+                              <h1 className='text-[12px] text-gray-500'>{data?.size}</h1>
                             </div>
                           </div>
-                          <div className='flex justify-end items-center '>
-                            <h1 className='text-[14px]'>Rs {data?.unit_price}</h1>
+                          <div className='flex justify-end items-start '>
+                            <h1 className='text-[13px] text-gray-500'>Rs {data?.unit_price}</h1>
                           </div>
-                          <div className='flex justify-end items-center'>
-                            <h1 className='text-[14px] pl-5'>{data?.quantity}</h1>
+                          <div className='flex justify-end items-start'>
+                            <h1 className='text-[13px] text-gray-500 pl-5'>x{data?.quantity}</h1>
                           </div>
-                          <div className='flex justify-end items-center'>
-                            <h1 className='text-[14px]'>Rs {data?.quantity_price}</h1>
+                          <div className='flex justify-end items-start'>
+                            <h1 className='text-[14px] font-[500]'>Rs {data?.quantity_price}</h1>
                           </div>
                         </div>
                       )
                     })
                   }
                 </div>
+
+
               </div>
             </div>
 
             {/* payment summary */}
-            <div className='w-full bg-white shadow-md rounded-[10px] p-4'>
+            <div className='w-full bg-white border shadow-md rounded-[10px] p-4'>
               <div className='w-full'>
                 <h1 className='text-[16px] font-[500]'>Payment Summary</h1>
               </div>
               <div className='w-full pt-5'>
 
                 {/* details */}
-                <div className=' flex flex-col'>
+                <div className=' flex flex-col gap-[4px]'>
                   <div className='flex justify-between w-full'>
-                    <h1 className='text-[13px]'>Subtotal</h1>
-                    <h1 className='text-[13px]'>{orderData?.payment_info?.shipping}</h1>
+                    <h1 className='text-[13px] text-gray-500'>Subtotal <span className='text-gray-400 text-[11px]'>&#40;{orderData?.items?.length} items&#41;</span></h1>
+                    <h1 className='text-[13px] text-gray-500'>{orderData?.payment_info?.shipping}</h1>
                   </div>
                   <div className='flex justify-between w-full'>
-                    <h1 className='text-[13px]'>Delivery Charges</h1>
-                    <h1 className='text-[13px]'>Rs 86</h1>
+                    <h1 className='text-[13px] text-gray-500'>Delivery Charges</h1>
+                    <h1 className='text-[13px] text-gray-500'>Rs 86</h1>
                   </div>
                   <div className='flex justify-between w-full pb-2'>
-                    <h1 className='text-[13px]'>Tax</h1>
-                    <h1 className='text-[13px]'>Rs 100</h1>
+                    <h1 className='text-[13px] text-gray-500'>Tax</h1>
+                    <h1 className='text-[13px] text-gray-500'>Rs 100</h1>
                   </div>
                   <div className='flex justify-between w-full pt-2 border-t'>
                     <h1 className='text-[14px] font-[600]'>Order Total</h1>
@@ -205,38 +224,83 @@ const OrdersEditPage = () => {
           {/* right flex */}
           <div className='w-full md:w-[35%]'>
 
-            <div className='w-full bg-white shadow-md border rounded-[10px] p-4'>
-              <div className='w-full mb-5'>
-                <h1 className='text-[16px] font-[500]'>Customer Details</h1>
+
+            {/* customer details and delivery status toggle */}
+            <div className='w-full bg-white shadow-md border rounded-[10px] py-4'>
+
+
+              {/* delivery status toggle */}
+              <div className='w-full flex justify-end items-center px-3 pb-6 border-b'>
+                <div className='w-full flex gap-6 justify-between items-center relative'>
+                  <div className='w-fit'>
+                    <h1 className={`text-[12px] text-gray-400`}>Delivery Status</h1>
+                  </div>
+                  <div className='w-fit'>
+                    <div className='w-fit flex items-center gap-4 rounded-[10px] p-1 px-2'>
+                      <span className={`text-[14px] font-[500] ${orderData?.status === 'Delivered' ? 'text-[#00ac69]' : orderData?.status === 'Dispatched' ? 'text-[#303030]' : orderData?.status === 'Canceled' ? 'text-[#FF0000]' : orderData?.status === 'Returned' ? 'text-[#e99f15]' : ''}`}>{orderData?.status}</span>
+                      <button className='bg-[#208a48] rounded-[8px] shadow-md' onClick={() => setStatusDropdownToggle(!statusDropdownToggle)}><img src={arrow_down} className={`w-full max-w-[24px] transition-all duration-300 ${statusDropdownToggle ? 'rotate-180' : ''}`} alt="" /></button>
+                    </div>
+                  </div>
+
+                  {/* status dropdown */}
+                  <div className={`w-[60%] bg-white rounded-[10px] z-[550] shadow-md absolute overflow-hidden top-[100%] right-0 transition-all duration-300 ${statusDropdownToggle ? 'h-[147px] ease-in border' : 'h-0 ease-out'}`}>
+                    <div className='w-full'>
+                      {
+                        orderData?.status_list?.map((data, i) => (
+                          <div key={i} className=' px-4 py-2 border-b hover:bg-gray-100 cursor-pointer hover:font-[500] transition-all duration-300' onClick={() => {
+                            setOrderData({
+                              ...orderData,
+                              status: data?.status_name
+                            })
+                            setStatusDropdownToggle(false)
+                          }}>
+                            <h1 className='text-[13px]' style={{ color: data?.status_color }}>{data?.status_name}</h1>
+                          </div>
+                        ))
+                      }
+                    </div>
+                  </div>
+                  <div className={`fixed inset-0 z-[520] transition-all duration-300 ${statusDropdownToggle ? 'block' : 'hidden'}`} onClick={() => setStatusDropdownToggle(false)}>
+
+                  </div>
+                </div>
               </div>
+
+
+              {/* customer details */}
               <div className='w-full'>
-                <h1 className='text-[15px]'>{orderData?.contact_info?.first_name} {orderData?.contact_info?.last_name}</h1>
-              </div>
-              <div className='w-full border-b border-gray-300 pt-1 pb-4'>
-                <h1 className='text-[13px]'>{orderData?.contact_info?.email}</h1>
-                <h1 className='text-[13px]'>{orderData?.contact_info?.phone_number}</h1>
-              </div>
-              <div className='w-full border-b border-gray-300 py-4'>
-                <div className='w-full'>
-                  <h1 className='text-[16px] font-[500] pb-5'>Shipping Address</h1>
+                <div className='w-full my-4 px-4'>
+                  <h1 className='text-[16px] font-[500]'>Customer Details</h1>
                 </div>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.address_line_1}</h1>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.address_line_2}</h1>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.landmark}</h1>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.city}</h1>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.state}</h1>
-                <h1 className='text-[13px]'>{orderData?.shipping_info?.country}</h1>
-              </div>
-              <div className='w-full border-b border-gray-300 py-4'>
-                <div className='w-full'>
-                  <h1 className='text-[16px] font-[500] pb-5'>Billing Address</h1>
+                <div className='w-full px-4'>
+                  <h1 className='text-[15px] text-gray-600'>{orderData?.contact_info?.first_name} {orderData?.contact_info?.last_name}</h1>
                 </div>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.address_line_1}</h1>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.address_line_2}</h1>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.landmark}</h1>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.city}</h1>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.state}</h1>
-                <h1 className='text-[13px]'>{orderData?.billing_info?.country}</h1>
+                <div className='w-full px-4 border-b border-gray-300 pt-2 pb-4'>
+                  <h1 className='text-[13px] pt-[4px] text-gray-500'>{orderData?.contact_info?.email}</h1>
+                  <h1 className='text-[13px] pt-[4px] text-gray-500'>{orderData?.contact_info?.phone_number}</h1>
+                </div>
+                <div className='w-full px-4 border-b border-gray-300 py-4'>
+                  <div className='w-full'>
+                    <h1 className='text-[16px] font-[500] pb-4'>Shipping Address</h1>
+                  </div>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.address_line_1}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.address_line_2}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.landmark}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.city}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.state}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.shipping_info?.country}</h1>
+                </div>
+                <div className='w-full px-4 border-gray-300 pt-4'>
+                  <div className='w-full'>
+                    <h1 className='text-[16px] font-[500] pb-4'>Billing Address</h1>
+                  </div>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.address_line_1}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.address_line_2}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.landmark}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.city}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.state}</h1>
+                  <h1 className='text-[13px] text-gray-500 pt-[4px]'>{orderData?.billing_info?.country}</h1>
+                </div>
               </div>
             </div>
 
