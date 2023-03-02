0f4e14d056546809f5d4b0ae70cce70fb6076425
Aayan-ekoinfomatics <aayanjit@ekoinfomatics.com>
2023-03-02 11:24:44 +0530

    bug fixes

--- src/base_address/base_address.js
+++ src/base_address/base_address.js
@@ -1,4 +1,4 @@
-const VITE_BASE_ADDRESS = "http://192.168.1.23:8000/";
-// const VITE_BASE_ADDRESS = "https://realvedic-api.andaal.com/";
+// const VITE_BASE_ADDRESS = "http://192.168.1.23:8000/";
+const VITE_BASE_ADDRESS = "https://realvedic-api.andaal.com/";
 
 export { VITE_BASE_ADDRESS };
